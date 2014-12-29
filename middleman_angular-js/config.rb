5d3b9c95f28777f793d8426452552145678ca493
progrhyme <progrhyme@gmail.com>
2014-12-30 03:30:23 +0900

    make output HTML readable

--- middleman_angular-js/config.rb
+++ middleman_angular-js/config.rb
@@ -58,6 +58,8 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
+set :slim, { :pretty => true, :sort_attrs => false, :format => :html5 }
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment

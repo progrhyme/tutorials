fa4e4546b905d38439750fac759fd058cc98be80
progrhyme <progrhyme@gmail.com>
2015-01-03 17:40:57 +0900

    Deploy to GitHub Page

--- middleman_angular-js2/config.rb
+++ middleman_angular-js2/config.rb
@@ -75,4 +75,14 @@ configure :build do
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
+
+  # for GitHub Pages
+  activate :asset_host, :host => "/middleman-angular-tutorial2"
+end
+
+# Deploy to GitHub Pages
+activate :deploy do |deploy|
+  deploy.build_before = true
+  deploy.method = :git
+  deploy.branch = 'gh-pages'
 end

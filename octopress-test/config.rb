ca1d79f9564fd4a739dab8274d74b37142b8dff7
progrhyme <progrhyme@gmail.com>
2016-04-01 13:55:56 +0900

    first setup

---
+++ octopress-test/config.rb
@@ -0,0 +1,19 @@
+require 'sass-globbing'
+
+# Require any additional compass plugins here.
+project_type = :stand_alone
+
+# Publishing paths
+http_path = "/octopress-test/"
+http_images_path = "/octopress-test/images"
+http_generated_images_path = "/images"
+http_fonts_path = "/octopress-test/fonts"
+css_dir = "public/octopress-test/stylesheets"
+
+# Local development paths
+sass_dir = "sass"
+images_dir = "source/images"
+fonts_dir = "source/fonts"
+
+line_comments = false
+output_style = :compressed

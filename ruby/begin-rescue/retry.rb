684b60321aebb742c19dd75ad0f273c40c43ba26
progrhyme <progrhyme@gmail.com>
2016-12-03 22:29:24 +0900

    Add ruby retry.rb

---
+++ ruby/begin-rescue/retry.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+
+def retry_on_error(times: 3)
+  @__try__ ||= 0
+  yield
+rescue
+  @__try__ += 1
+  retry if @__try__ < times
+  raise
+end
+
+def retry_on_error2(times: 3)
+  try = 0
+  begin
+    yield
+  rescue
+    try += 1
+    retry if try < times
+    raise
+  end
+end
+
+@try = 0
+retry_on_error do
+  @try += 1
+  p @try
+  raise "Failed after #{@try} trials."
+end

15415dc05a31817d9f25e784b5c134ba243a7f1a
progrhyme <progrhyme@gmail.com>
2017-01-29 14:47:09 +0000

    [capistrano] Update capistarano & sshkit

--- capistrano/config/deploy.rb
+++ capistrano/config/deploy.rb
@@ -1,32 +1,30 @@
 # config valid only for current version of Capistrano
-#lock '3.4.0'
+lock "3.7.2"
 
-set :application, 'ec2it'
-set :repo_url, 'git@github.com:key-amb/ruby-ec2it.git'
+set :application, "ec2it"
+set :repo_url, "git@github.com:key-amb/ruby-ec2it.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/home/key-amb/ec2it'
+set :deploy_to, "#{Pathname.new(ENV['HOME']).join(fetch(:application))}"
 
-# Default value for :scm is :git
-# set :scm, :git
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
 
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
 
 # Default value for :pty is false
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+# append :linked_files, "config/database.yml", "config/secrets.yml"
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -34,23 +32,10 @@ set :deploy_to, '/home/key-amb/ec2it'
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
-set :message, fetch(:message, 'default message')
-
 namespace :deploy do
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
   task :foo do
-    on release_roles(:app) do
-      info 'Do something for :app server'
-      info fetch(:message)
+    on release_roles(:all) do
+      info 'Do something for all servers'
       execute :hostname
     end
   end

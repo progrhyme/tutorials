f42201795729fe1fc7835038477ee242b53f6fcb
progrhyme <progrhyme@gmail.com>
2020-05-30 23:57:26 +0900

    [rust/grrs] Use failure::ResultExt & exitfailure::ExitFailure

--- rust/grrs/src/main.rs
+++ rust/grrs/src/main.rs
@@ -1,3 +1,5 @@
+use exitfailure::ExitFailure;
+use failure::ResultExt;
 use structopt::StructOpt;
 
 /// Search for a pattern in a file and display the lines that contain it.
@@ -10,14 +12,11 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-#[derive(Debug)]
-struct CustomError(String);
-
-fn main() -> Result<(), CustomError> {
+fn main() -> Result<(), ExitFailure> {
     let args = Cli::from_args();
 
     let content = std::fs::read_to_string(&args.path)
-        .map_err(|err| CustomError(format!("Error reading `{}`: {}", &args.path.display(), err)))?;
+        .with_context(|_| format!("could not read file `{}`", &args.path.display()))?;
     for line in content.lines() {
         if line.contains(&args.pattern) {
             println!("{}", line);

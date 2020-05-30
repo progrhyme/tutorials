a7f01e230e8eef7ad09a2e7102f82e2870b3531f
progrhyme <progrhyme@gmail.com>
2020-05-30 16:56:05 +0900

    [rust/grrs] 1st impl. https://rust-cli.github.io/book/tutorial/impl-draft.html

--- rust/grrs/src/main.rs
+++ rust/grrs/src/main.rs
@@ -10,17 +10,12 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-//struct Cli {
-//    pattern: String,
-//    path: std::path::PathBuf,
-//}
-
 fn main() {
     let args = Cli::from_args();
-    //let pattern = std::env::args().nth(1).expect("no pattern given");
-    //let path = std::env::args().nth(2).expect("no path given");
-    //let args = Cli {
-    //    pattern: pattern,
-    //    path: std::path::PathBuf::from(path),
-    //};
+    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
 }
